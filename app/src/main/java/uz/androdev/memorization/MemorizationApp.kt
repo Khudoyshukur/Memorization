6580bd6c54c8700e2fb1952d62ba43e38875855a
Xudoyshukur <Khudoyshukur.Juraev.001@mail.ru>
2022-10-21 17:04:09 +0500

    Configure build.gradle files. Add Hilt dependencies

---
+++ app/src/main/java/uz/androdev/memorization/MemorizationApp.kt
@@ -0,0 +1,15 @@
+package uz.androdev.memorization
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by: androdev
+ * Date: 21-10-2022
+ * Time: 4:57 PM
+ * Email: Khudoyshukur.Juraev.001@mail.ru
+ */
+
+@HiltAndroidApp
+class MemorizationApp : Application() {
+}
